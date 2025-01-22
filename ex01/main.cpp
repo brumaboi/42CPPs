d1f48df9467a3d5d091f1301970371641c238daf
brumaboi <brumastefan8@gmail.com>
2025-01-22 13:44:33 +0100

    done main function for the phonebook

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main()
+{
+    std::string command;
+
+    std::cout << "Welcome to my Awesome PhoneBook!" << std::endl;
+    std::cout << "Available commands: ADD, SEARCH, EXIT" << std::endl;
+    while(1)
+    {
+        std::cout << "Enter command: ";
+        std::getline(std::cin, command);
+        if (command == "ADD")
+        {
+            //function to do add
+        }
+        else if (command == "SEARCH")
+        {
+            //function to do search
+        }
+        else if (command == "EXIT")
+        {
+            std::cout << "Bye! Bye! We're closing shop!" << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid command! Try using: ADD, SEARCH, EXIT" << std::endl;
+        }
+    }
+    return 0;
+}
