082ca72ee02dec1d26a8868110fa2122b4e77660
brumaboi <brumastefan8@gmail.com>
2025-03-13 12:39:31 +0100

    started CPP03, created the class, constructor, destructor

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -10,3 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+    : name(name),
+      hitPoints(10),
+      energyPoints(10),
+      attackDamage(0)
+{
+    std::cout << "ClapTrap " << name << " is constructed!" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src)
+{
+    *this = src;
+    std::cout << "ClapTrap " << name << " is constructed!" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap " << name << " is destructed!" << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &src)
+{
+    if (this != &src)
+    {
+        this->name = src.name;
+        this->hitPoints = src.hitPoints;
+        this->energyPoints = src.energyPoints;
+        this->attackDamage = src.attackDamage;
+    }
+    return (*this);
+}
+
