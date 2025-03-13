beb283eb6bc544eb93bf97b5c5af6789dc0cde80
brumaboi <brumastefan8@gmail.com>
2025-03-13 15:33:41 +0100

    added files from the first exercises to the second one, also updated files

---
+++ CPP03/ex01/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 11:48:58 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/13 11:48:58 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string     name;
+        int    hitPoints;
+        int    energyPoints;
+        int    attackDamage;
+
+    public:
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap &src);
+        ~ClapTrap();
+        ClapTrap &operator=(const ClapTrap &src);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
+
+#endif
--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/13 11:49:50 by marvin            #+#    #+#             */
-/*   Updated: 2025/03/13 11:49:50 by marvin           ###   ########.fr       */
+/*   Created: 2025/03/13 11:49:00 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/13 11:49:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAVTRAP_HPP
-# define SCAVTRAP_HPP
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-#include <iostream>
-
-
-#endif
+int main()
+{
+    ClapTrap clap("Denis");
+    
+    clap.attack("Albert");
+    clap.takeDamage(3);
+    clap.beRepaired(1);
+    return (0);
+}
