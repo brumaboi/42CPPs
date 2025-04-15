d55fb1591234f38fc10659f285f997c03ad19739
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2025-04-15 19:05:57 +0200

    preped all files in ex05 ex06 also created the class in ex05 as indicated by the subject

---
+++ CPP01/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 19:00:33 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/15 19:00:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl{
+
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        Harl(void);
+        ~Harl(void);
+        void complain( std::string level );
+};
+
+#endif
