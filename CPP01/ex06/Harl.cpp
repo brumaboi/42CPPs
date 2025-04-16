a9e3d00b40f73f469a65ad3b2c86cd6679854e72
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2025-04-16 10:56:43 +0200

    fixed level incrementation + used switch instead of if-elses, also intentionall fallthrough  to achieve what the subject requires

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -30,25 +30,29 @@ Harl::~Harl()
 
 void Harl::debug( void )
 {
-    std::cout << "[DEBUG]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[DEBUG] " << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info( void )
 {
-    std::cout << "[INFO]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[INFO] " << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning( void )
 {
-    std::cout << "[WARNING]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[WARNING] " << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::error( void )
 {
-    std::cout << "[ERROR]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[ERROR] " << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -59,7 +63,23 @@ void Harl::complain( std::string level )
     i = 0;
     while (i < 4)
     {
-        if (levels[i++] == level)
+        if (levels[i] == level)
+            break ;
+        i++;
+    }
+    switch (i)
+    {
+        case 0:
+            (this->*f[0])();
+            //fall through
+        case 1:
+            (this->*f[1])();
+            //fall through
+        case 2:
+            (this->*f[2])();
+            //fall through
+        case 3:
+            (this->*f[3])();
             break ;
     }
 }
