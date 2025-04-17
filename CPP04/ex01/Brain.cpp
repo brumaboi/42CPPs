f2f8f71f52acf2b323a90a0dd04c4bea204941c7
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2025-04-17 12:59:37 +0200

    implemented brain class, updated makefile

--- CPP04/ex01/Brain.cpp
+++ CPP04/ex01/Brain.cpp
@@ -11,3 +11,30 @@
 /* ************************************************************************** */
 
 #include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Brain default constructor called" << std::endl;
+}
+
+Brain::Brain(const Brain &other)
+{
+    std::cout << "Brain copy constructor called" << std::endl;
+    *this = other;
+}
+
+Brain::~Brain()
+{
+    std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain &Brain::operator=(const Brain &other)
+{
+    std::cout << "Brain assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        for (int i = 0; i < 100; i++)
+            ideas[i] = other.ideas[i];
+    }
+    return (*this);
+}
