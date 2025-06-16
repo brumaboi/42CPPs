1189252bd23fe17035b76d12d458faa650fba9bb
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2025-06-16 14:49:27 +0200

    added class as indicated by the subject

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -10,3 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class Bureaucrat {
+
+    private:
+        int grade;
+        std::string const name;
+        void setGrade(int grade);
+
+        
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat &operator=(const Bureaucrat &other);
+        ~Bureaucrat();
+
+        const std::string &getName() const;
+        int getGrade() const;
+
+        void incrementGrade();
+        void decrementGrade();
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char *what() const throw() override;
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char *what() const throw() override;
+        };
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif
