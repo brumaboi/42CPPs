2866ed71660d8781f5583de99392cb7efe42f1ea
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2025-06-16 15:42:56 +0200

    implemented tests for ex00

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -14,5 +14,53 @@
 
 int main()
 {
-    
+    std::cout << "Test 1: " << std::endl;
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 1);
+        std::cout << bureaucrat << std::endl;
+        bureaucrat.decrementGrade();
+        std::cout << bureaucrat << std::endl;
+        bureaucrat.incrementGrade();
+        std::cout << bureaucrat << std::endl;
+        bureaucrat.incrementGrade(); // This should throw an exception
+        std::cout << bureaucrat << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl << "Test 2: " << std::endl;
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 150);
+        std::cout << bureaucrat << std::endl;
+        bureaucrat.decrementGrade(); // This should throw an exception
+        std::cout << bureaucrat << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl << "Test 3: " << std::endl;
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 0); // This should throw an exception
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << std::endl << "Test 4: " << std::endl;
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 151); // This should throw an exception
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }    
 }
