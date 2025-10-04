050d47b5716cb26ebfe104fbe4d71b8fb1bf62c3
Stefan-Andrei Bruma <67876621+brumaboi@users.noreply.github.com>
2025-10-04 23:38:52 +0200

    ex02: added empty classes and implemented Base class as indicated by the subject

--- CPP06/ex02/Base.cpp
+++ CPP06/ex02/Base.cpp
@@ -10,3 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+Base::~Base()
+{
+}
+
+Base *generate(void)
+{
+    static bool first = true;
+    if (first)
+    {
+        srand(time(NULL));
+        first = false;
+    }
+    int random = rand() % 3;
+    if (random == 0)
+        return new A();
+    else if (random == 1)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base *p)
+{
+
+    std::cout << "Pointer ttype: ";
+
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Unknown" << std::endl;
+}
+
+void identify(Base& p)
+{
+// to be implemented
+}
--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -10,3 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once 
+
+class Base {
+
+public:
+    virtual ~Base() {}
+};
+
+Base* generate(void);
+void identify(Base* p);
+void identify(Base& p);
