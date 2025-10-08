42f26c333b0fc001f4bb5e045ffa53de283ad6bb
Stefan-Andrei Bruma <67876621+brumaboi@users.noreply.github.com>
2025-10-08 22:19:18 +0200

    ex01: removed extra file; implemented function template iter as indicated by the subject; added tests;

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -10,3 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+
+template <typename T, typename F>
+void iter(T* array, size_t length, F function)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        function(array[i]);
+    }
+}
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -10,3 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+int main()
+{
+    int intArr[] = {1, 2, 3, 4, 5};
+    char charArr[] = {'a', 'b', 'c', 'd', 'e'};
+    std::string strArr[] = {"one", "two", "three", "four", "five"};
+
+    iter(intArr, 5, [](int &n) { std::cout << n << std::endl; });
+    iter(charArr, 5, [](char &c) { std::cout << c << std::endl; });
+    iter(strArr, 5, [](std::string &s) { std::cout << s << std::endl; });
+
+    return 0;
+}
