38c9a73afc11809132f43ef8c081c353a40b74c7
Stefan-Andrei Bruma <brumastefan8@gmail.com>
2026-02-18 14:35:09 +0100

    implemented class&methods as required by the subject

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -10,3 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(unsigned int N) : maxSize(N) {}
+
+Span::Span(const Span &other) : numbers(other.numbers), maxSize(other.maxSize) {}
+
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other) {
+        numbers = other.numbers;
+        maxSize = other.maxSize;
+    }
+    return *this;
+}
+
+void Span::addNumber(int number)
+{
+    if (numbers.size() >= maxSize)
+        throw std::length_error("Span is already full");
+    numbers.push_back(number);
+}
+
+int Span::shortestSpan() const
+{
+    if (numbers.size() < 2)
+        throw std::length_error("Span too short");
+
+    std::vector<int> sorted = numbers;
+    std::sort(sorted.begin(), sorted.end());
+    int shortest = sorted[1] - sorted[0];
+    for (size_t i = 2; i < sorted.size(); ++i) {
+        int span = sorted[i] - sorted[i - 1];
+        if (span < shortest)
+            shortest = span;
+    }
+    return shortest;
+}   
+
+int Span::longestSpan() const
+{
+    if (numbers.size() < 2)
+        throw std::length_error("Span too short");
+
+    std::vector<int> sorted = numbers;
+    std::sort(sorted.begin(), sorted.end());
+
+    return sorted.back() - sorted.front();
+}
+
+unsigned int Span::size() const
+{
+    return numbers.size();
+}
+
+std::vector<int> const &Span::getNumbers() const
+{
+    return numbers;
+}
+
+std::ostream &operator<<(std::ostream &os, const Span &span)
+{
+    os << "Span(" << span.size() << "/" << span.getNumbers().size() << "): {";
+    std::vector<int> const &nums = span.getNumbers();
+
+    for (size_t i = 0; i < nums.size(); ++i)
+    {
+        os << nums[i];
+        if (i < nums.size() - 1)
+            os << ", ";
+    }
+    os << "}";
+    return os;
+}
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -10,3 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+
+class Span {
+
+    private:
+        std::vector<int> numbers;
+        unsigned int maxSize;
+
+    public:
+        Span() = delete;
+        Span(unsigned int N);
+        Span(const Span &other);
+        Span &operator=(const Span &other);
+        ~Span() = default;
+
+        void addNumber(int number);
+        template<typename InputIt>
+        void addNumber(InputIt first, InputIt last)
+        {
+            if(std::distance(first, last) + numbers.size() > maxSize)
+                throw std::length_error("Adding these numbers would exceed the maximum size of the Span.");
+            numbers.insert(numbers.end(), first, last);
+        }
+
+        int shortestSpan() const;
+        int longestSpan() const;
+
+        unsigned int size() const;
+        std::vector<int> const &getNumbers() const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Span &span);
