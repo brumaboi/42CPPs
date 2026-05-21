9d486cb68e4257fc89050cf6fe4b817876b76f8d
Stefan-Andrei Bruma <67876621+brumaboi@users.noreply.github.com>
2026-05-21 04:09:14 +0200

    added const to ex01 length to pass the subject

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -15,7 +15,7 @@
 #include <iostream>
 
 template <typename T, typename F>
-void iter(T* array, size_t length, F function)
+void iter(T* array, size_t const length, F function)
 {
     for (size_t i = 0; i < length; ++i)
     {
